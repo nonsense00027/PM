a9ad674f4a02429a4add6655a37ac039afacad67
nonsense00027 <caneteneilfred@gmail.com>
2020-03-05 18:52:24 -0800

    Layouts for different departments changed

--- resources/views/accountability/index2.blade.php
+++ resources/views/accountability/index2.blade.php
@@ -101,7 +101,7 @@
                 <tbody>
                 @foreach ($accountabilities as $accountability)
                   @if($accountability->status == 'false')
-                  <tr style="color:#d12002">
+                  <tr class=".danger">
                   @elseif($accountability->status == 'true')
                   <tr>
                   @endif
@@ -115,222 +115,22 @@
                         <i class="fas fa-pen" title="Edit user information"></i>
                       </a>
                       <!-- EDIT MODAL -->
-                      <div class="modal fade bd-example-modal-lg printModal" id="exampleModal-{{$accountability->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header bg-primary">
-                              <h5 class="modal-title text-gray-100" id="exampleModalLabel">Modify {{$accountability->name}}'s user information</h5>
-                              <button type="button" class="close text-gray-100" data-dismiss="modal" aria-label="Close">
-                                <span  aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <form id="editForm1" action="/accountabilities/{{$accountability->id}}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <div class="modal-body">
-                                  <div class="form-row">
-                                    <div class="col-md-1 mb-3">
-                                      <label>ID</label>
-                                      <input readonly type="text" name="id" id="editid" class="form-control bg-outline-success" value="{{$accountability->id}}" required>
-                                    </div>
-
-                                    <div class="col-md-4 mb-3">
-                                      <label>Full Name</label>
-                                      <input type="text" name="name" id="editname" class="form-control bg-outline-success" value="{{$accountability->name}}" required>
-                                    </div>
-
-                                    <div class="col-md-4 mb-3">
-                                      <label>Company</label>
-                                      <input type="text" name="company" id="editcompany" class="form-control bg-outline-success" value="{{$accountability->company}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Designation</label>
-                                      <input type="text" name="designation" id="editdesignation" class="form-control" value="{{$accountability->designation}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                    <div class="col-md-3 mb-3">
-                                      <label>Computer Name</label>
-                                      <input type="text" name="computer_name" id="editcomputer_name" class="form-control" value="{{$accountability->computer_name}}" required>
-                                    </div>
-                                    <div class="col-md-3 mb-3">
-                                      <label>Location</label>
-                                      <input type="text" name="location" id="editlocation" class="form-control" value="{{$accountability->location}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Local User</label>
-                                      <input type="text" name="local_user" id="editlocal_user" class="form-control" value="{{$accountability->local_user}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Local Password</label>
-                                      <input type="text" name="local_password" id="editlocal_password" class="form-control" value="{{$accountability->local_password}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                    <div class="col-md-3 mb-3">
-                                      <label>Domain Account</label>
-                                      <input type="text" name="domain_acc" id="editdomain_acc" class="form-control" value="{{$accountability->domain_acc}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Domain Password</label>
-                                      <input type="text" name="domain_pass" id="editdomain_pass" class="form-control" value="{{$accountability->domain_pass}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>IP Address</label>
-                                      <input type="text" name="ip_address" id="editip_address" class="form-control" value="{{$accountability->ip_address}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>MAC Address</label>
-                                      <input type="text" name="mac_address" id="editmac_address" class="form-control" value="{{$accountability->mac_address}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                      <label>Email</label>
-                                      <input type="text" name="email" id="editemail" class="form-control" value="{{$accountability->email}}" required>
-                                  </div>
-                                  <input type="hidden" name="status" value="true">
-                                  <br>
-                                  <div class="form-row">
-                                      <label id="remarksForPrint">Remarks</label>
-                                      <input type="text" name="remark" id="remark" class="form-control" required>
-                                  </div>
-                              </div>
-                            
-                              <div class="modal-footer">
-                                  <button id="printBtn" type="button" class="btn btn-success" onclick="printJS({ printable: 'editForm1', type: 'html', documentTitle: 'FTC Group of Companies | Accountability Form of {{$accountability->name}}', css: '/css/print.css', honorColor: 'true', ignoreElements: ['saveBtn','cancelBtn','remarksForPrint','printBtn'], gridStyle: 'border: 1px solid lightgray; margin-bottom: -1px;', targetStyle: 'null' })">
-                                    <i class="fas fa-print" title="Edit user information"></i>&nbsp&nbspPrint
-                                  </button>
-
-                                  <!-- <button type="button" class="btn btn-success" id="printButton">
-                                    <i class="fas fa-print" title="Edit user information"></i>&nbsp&nbspPrint
-                                  </button> -->
-                                  <button id="saveBtn" type="submit" class="btn btn-primary">Save changes</button>
-                                  <button id="cancelBtn" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  
-                              </div>
-                            </form>
-                          </div>
-                        </div>
-                      </div>  
-
+                      @if(Auth::user()->name == 'HR')
+                        @include('layouts.edit.hr')
+                      @elseif(Auth::user()->name == 'Information Technology')
+                        @include('layouts.edit.it')
+                      @endif
                       <!-- INVENTORY FUNCTION -->
 
                       <a href="#" data-target="#inventoryModal-{{$accountability->id}}" class="edit mx-3" data-toggle="modal" title="User Accountability" data-placement="left" >
                         <i class="fas fa-th-list" title="User Accountability"></i>
                       </a>
                       <!-- INVENTORY MODAL -->
-                      <div class="modal fade" id="inventoryModal-{{$accountability->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog modal-lg" role="document">
-                          <div class="modal-content">
-                            <div class="modal-header bg-primary">
-                              <h5 class="modal-title text-gray-100" id="exampleModalLabel">{{$accountability->name}}'s Inventory</h5>
-                              <button type="button" class="close text-gray-100" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                              </button>
-                            </div>
-                            <form id="editForm2" action="/inventories/{{$accountability->id}}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <div class="modal-body">
-                                  <input type="hidden" name="id" id="editid" class="form-control" value="{{$accountability->id}}" required>
-                                    
-                                  <div class="form-row">
-                                    <div class="col-md-3 mb-3">
-                                      <label>Motherboard</label>
-                                      <input type="text" name="motherboard" id="editcomputer_name" class="form-control" value="{{$accountability->motherboard}}" required>
-                                    </div>
-                                    <div class="col-md-3 mb-3">
-                                      <label>CPU</label>
-                                      <input type="text" name="cpu" id="editlocation" class="form-control" value="{{$accountability->cpu}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>HDD</label>
-                                      <input type="text" name="hdd" id="editlocal_user" class="form-control" value="{{$accountability->hdd}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Memory</label>
-                                      <input type="text" name="memory" id="editlocal_password" class="form-control" value="{{$accountability->memory}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                    <div class="col-md-3 mb-3">
-                                      <label>Monitor</label>
-                                      <input type="text" name="monitor" id="editdomain_acc" class="form-control" value="{{$accountability->monitor}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Case</label>
-                                      <input type="text" name="case" id="editdomain_pass" class="form-control" value="{{$accountability->case}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Keyboard</label>
-                                      <input type="text" name="keyboard" id="editip_address" class="form-control" value="{{$accountability->keyboard}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Mouse</label>
-                                      <input type="text" name="mouse" id="editmac_address" class="form-control" value="{{$accountability->mouse}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                    <div class="col-md-3 mb-3">
-                                      <label>Video Card</label>
-                                      <input type="text" name="video_card" id="editdomain_acc" class="form-control" value="{{$accountability->video_card}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Power Supply</label>
-                                      <input type="text" name="power_supply" id="editdomain_pass" class="form-control" value="{{$accountability->power_supply}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Printer</label>
-                                      <input type="text" name="printer" id="editip_address" class="form-control" value="{{$accountability->printer}}" required>
-                                    </div>
-
-                                    <div class="col-md-3 mb-3">
-                                      <label>Telephone</label>
-                                      <input type="text" name="telephone" id="editmac_address" class="form-control" value="{{$accountability->telephone}}" required>
-                                    </div>
-                                  </div>
-
-                                  <div class="form-row">
-                                      <label>Remarks</label>
-                                      <input type="text" name="remark" id="remark" class="form-control" required>
-                                  </div>
-                              </div>
-
-                              <div class="modal-footer">
-
-                                <button id="printBtn" type="button" class="btn btn-success" onclick="printJS({ printable: 'editForm2', type: 'html', header: 'Inventory Form of {{$accountability->name}}', css: '/css/sb-admin-2.css' })">
-                                    <i class="fas fa-print" title="Edit user information"></i>&nbsp&nbspPrint
-                                  </button>
-
-                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  @if(Auth::user()->role == 'Admin')
-                                  <button type="submit" class="btn btn-primary">Save changes</button>
-                                  @endif
-                              </div>
-                            </div>
-                            </form>
-                          </div>
-                        </div>
-                      </div>
-                      
+                      @if(Auth::user()->name == 'Information Technology')
+                        @include('layouts.inventory.it')
+                      @else
+                        @include('layouts.inventory.other')
+                      @endif
                       <!-- END OF INVENTORY MODAL -->
 
                       <!-- LOGS FUNCTION -->
---
+++ resources/views/layouts/inventory/other.blade.php
@@ -0,0 +1,94 @@
+<div class="modal fade" id="inventoryModal-{{$accountability->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+        <div class="modal-header bg-primary">
+            <h5 class="modal-title text-gray-100" id="exampleModalLabel">{{$accountability->name}}'s Inventory</h5>
+            <button type="button" class="close text-gray-100" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <form id="editForm2" action="/inventories/{{$accountability->id}}" method="POST">
+        @csrf
+        @method('PATCH')
+        <div class="modal-body">
+                <input type="hidden" name="id" id="editid" class="form-control" value="{{$accountability->id}}" required>
+                
+                <div class="form-row">
+                <div class="col-md-3 mb-3">
+                    <label>Motherboard</label>
+                    <input readonly type="text" name="motherboard" id="editcomputer_name" class="form-control" value="{{$accountability->motherboard}}" required>
+                </div>
+                <div class="col-md-3 mb-3">
+                    <label>CPU</label>
+                    <input readonly type="text" name="cpu" id="editlocation" class="form-control" value="{{$accountability->cpu}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>HDD</label>
+                    <input readonly type="text" name="hdd" id="editlocal_user" class="form-control" value="{{$accountability->hdd}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Memory</label>
+                    <input readonly type="text" name="memory" id="editlocal_password" class="form-control" value="{{$accountability->memory}}" required>
+                </div>
+                </div>
+
+                <div class="form-row">
+                <div class="col-md-3 mb-3">
+                    <label>Monitor</label>
+                    <input readonly type="text" name="monitor" id="editdomain_acc" class="form-control" value="{{$accountability->monitor}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Case</label>
+                    <input readonly type="text" name="case" id="editdomain_pass" class="form-control" value="{{$accountability->case}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Keyboard</label>
+                    <input readonly type="text" name="keyboard" id="editip_address" class="form-control" value="{{$accountability->keyboard}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Mouse</label>
+                    <input readonly type="text" name="mouse" id="editmac_address" class="form-control" value="{{$accountability->mouse}}" required>
+                </div>
+                </div>
+
+                <div class="form-row">
+                <div class="col-md-3 mb-3">
+                    <label>Video Card</label>
+                    <input readonly type="text" name="video_card" id="editdomain_acc" class="form-control" value="{{$accountability->video_card}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Power Supply</label>
+                    <input readonly type="text" name="power_supply" id="editdomain_pass" class="form-control" value="{{$accountability->power_supply}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Printer</label>
+                    <input readonly type="text" name="printer" id="editip_address" class="form-control" value="{{$accountability->printer}}" required>
+                </div>
+
+                <div class="col-md-3 mb-3">
+                    <label>Telephone</label>
+                    <input readonly type="text" name="telephone" id="editmac_address" class="form-control" value="{{$accountability->telephone}}" required>
+                </div>
+                </div>
+            </div>
+
+            <div class="modal-footer">
+
+            <button id="printBtn" type="button" class="btn btn-success" onclick="printJS({ printable: 'editForm2', type: 'html', header: 'Inventory Form of {{$accountability->name}}', css: '/css/sb-admin-2.css' })">
+                <i class="fas fa-print" title="Edit user information"></i>&nbsp&nbspPrint
+                </button>
+
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+        </form>
+        </div>
+    </div>
+    </div>
